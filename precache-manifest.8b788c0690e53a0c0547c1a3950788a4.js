1040c3ab2a9aeded5b2f4f5ebfaca3117dd74f60
Andrew Osborn <andrewosborn101@gmail.com>
2019-04-30 23:12:15 -0500

    Updates

---
+++ precache-manifest.8b788c0690e53a0c0547c1a3950788a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0405ff0ccf3676466cb4f72d24447e4d",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "306b7d3663cdee2c1afe",
+    "url": "/ClickyGame/static/css/main.5f1b36c6.chunk.css"
+  },
+  {
+    "revision": "8206ddf9d351031d494c",
+    "url": "/ClickyGame/static/js/2.cf0d62a8.chunk.js"
+  },
+  {
+    "revision": "306b7d3663cdee2c1afe",
+    "url": "/ClickyGame/static/js/main.ec09d199.chunk.js"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  }
+]);
